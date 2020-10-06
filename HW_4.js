3ed57da28a22c0e1a7055bc17715d27a2afeefb8
ismyloven <58706296+ismyloven@users.noreply.github.com>
2020-10-06 15:00:49 +0300

    Update HW_4.js

--- HW_4.js
+++ HW_4.js
@@ -1,22 +1,22 @@
 // первое задание
 
-const apple = 12.50;
-const discount = 15;
+const apple = 12.500000000000;
+const discount = 100;
 var priceList = '';
+let ApplePrice;
+let newApplePrice;
 
-if (discount > 0 && discount <= 100 && typeof (discount) === "number") {
-	var ApplePrice = apple - (apple * discount / 100);
-	var newApplePrice = Math.floor(ApplePrice * 100) / 100;
-	console.log(priceList = `Яблоки украинские.
+
+
+if (discount >= 0 && discount <= 100 && typeof (discount) === "number" ) {
+  ApplePrice = apple - (apple * discount / 100);
+  newApplePrice = (Math.floor(ApplePrice * 100) / 100).toFixed(2);
+  console.log(priceList = `Яблоки украинские.
 цена: ${newApplePrice} грн`);
-
-} else if (discount === 0) {
-	console.log(priceList = `Яблоки украинские.
-цена: ${apple} грн`);
 
 } else {
 
-	console.log("Вы ввели неправильную скидку");
+  console.log("Вы ввели неправильную скидку");
 }
 
 // Второе задание
@@ -26,14 +26,13 @@ var cord = "Ismailov xochet stat programmistom";
 
 if (typeof (cord) === "string" && cord.length < 10) {
 
-	for (var i = 0; i < cord.length; i++) {
-		console.log(cord.charAt(i));
-	}
+  for (var i = 0; i < cord.length; i++) {
+    console.log(cord.charAt(i));
+  }
 
 } else if (typeof (cord) === "number" || cord === undefined) {
-	console.log('Введите значение, используя буквы');
+  console.log('Введите значение, используя буквы');
 } else {
-	console.log(cord.substring(0, 7));
-	console.log(cord.substring(9));
+  console.log(cord.substring(0, 7));
+  console.log(cord.substring(9));
 }
-
