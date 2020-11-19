45405de6d3ce5cdc097a4574b1740a01c48816db
Nail Ismailov <dd030789ine@gmail.com>
2020-11-19 17:23:50 +0200

    cache

--- HW_17_cache.js
+++ HW_17_cache.js
@@ -1,5 +1,3 @@
-
-
 function complexFunction(arg1, arg2) {
 	return arg1 + arg2;
 }
@@ -8,14 +6,14 @@ function complexFunction(arg1, arg2) {
 function cache(func) {
 	var obj = {};
 	return function () {
-		var args = [...arguments];
+		var args = [...arguments].reduce((sum, currentEl) => (sum + currentEl));
 		if (args in obj) {
-			console.log('Fetching from cache');
+			console.log('result from cache');
 			return obj[args];
 		} else {
-			var value = func.apply(this, args);
+			var value = func.apply(this, arguments);
 			obj[args] = value;
-			console.log('Calculating result');
+			console.log('сalculating result');
 			return value;
 		}
 	}
@@ -27,6 +25,6 @@ var cachedFunction = cache(complexFunction);
 console.log(cachedFunction('foo', 'bar'));
 console.log(cachedFunction('foo', 'bar'));
 console.log(cachedFunction('foo', 'baz'));
-console.log(cachedFunction(9, 81));
-console.log(cachedFunction(81, 9));
+console.log(cachedFunction(5, 10));
+console.log(cachedFunction(10, 5));
 console.log(cachedFunction(10, 15));
